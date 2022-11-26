791c6128ab5b95d018ccb85714bde8688ab79617
James Njenga <knjenga94@gmail.com>
2022-11-26 09:16:46 +0300

    Completed Lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length:{minimum:250}
+    validates :summary, length:{maximum:250}
+    validates :category, inclusion:{in:%w(Fiction Non-Fiction), message: "is not included in the list"}
+
+    # validate :title_is_clickbait
+
+    # def title_is_clickbait
+    # if !title["Won't Believe"] 
+    #     errors.add(:title,"Not clickbaitey")
+    #     #||!title["Secret"]||!title["Guess"]||!title["Top [number]"]
+    # end
+    # end
 end
